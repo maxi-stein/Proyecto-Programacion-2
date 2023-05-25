4bb0e08c49a2c1647c222ae037ae0a25259e1f57
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-05-25 13:30:38 -0300

    Agregado de Atributos y Constructor a Usuario

--- proyecto-programacion2/src/holding/Usuario.java
+++ proyecto-programacion2/src/holding/Usuario.java
@@ -1,4 +1,13 @@
 package holding;
 
 public abstract class Usuario {
+    private String nombre;
+    private String direccion;
+    private String pass;
+
+    public Usuario(String nombre, String direccion, String pass) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.pass = pass;
+    }
 }
