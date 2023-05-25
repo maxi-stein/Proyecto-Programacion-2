a6a3f6c5af3ed0a55de137e92b8b2a377360bd66
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-05-25 18:11:18 -0300

    Implementacion Clase Asesor

--- proyecto-programacion2/src/holding/Asesor.java
+++ proyecto-programacion2/src/holding/Asesor.java
@@ -1,2 +1,16 @@
-package holding;public class Asesor {
+package holding;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Asesor extends Usuario{
+    private String titulacion;
+    private LocalDate fechaInicio;
+    private ArrayList<AREAS_MERCADO> mercadosCubiertos;
+
+    public Asesor(String nombre, String direccion, String pass, String titulacion, LocalDate fechaInicio) {
+        super(nombre, direccion, pass);
+        this.titulacion = titulacion;
+        this.fechaInicio = fechaInicio;
+    }
 }
