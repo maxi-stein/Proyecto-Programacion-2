4582241645267a74b254d999eeb11b6be90299ad
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-05-25 18:11:51 -0300

    Modificacion clase BaseDatosHolding

--- proyecto-programacion2/src/holding/BaseDatosHolding.java
+++ proyecto-programacion2/src/holding/BaseDatosHolding.java
@@ -1,4 +1,49 @@
 package holding;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BaseDatosHolding {
+    private ArrayList<Usuario> usuarios;
+
+    public BaseDatosHolding(){
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Admin("Mati","Calle Falsa","123"));
+        usuarios.add(new Admin("Maxi","Calle Verdadera","321"));
+    }
+    public  Usuario iniciarSesion(){
+        System.out.println("Usuario:");
+        String user = Consola.leerString();
+        System.out.println("Contraseña:");
+        String pass = Consola.leerString();
+
+        return autentificarUsuario(user,pass);
+    }
+
+    private Usuario autentificarUsuario(String user,String pass){
+        Usuario usuarioEncontrado = null;
+        for(Usuario usuario : usuarios){
+            if(usuario.validarNombre(user)){
+                if(usuario.validarPass(pass)){
+                    usuarioEncontrado = usuario;
+                }
+            }
+        }
+        return usuarioEncontrado;
+    }
+
+    private class Consola {
+        private Consola(){};
+        private static Scanner input = new Scanner(System.in);
+
+        public static int leerNum(){
+            String numero = input.nextLine();
+            return Integer.parseInt(numero);
+        }
+
+        public static String leerString(){
+            return input.nextLine();
+        }
+
+    }
 }
