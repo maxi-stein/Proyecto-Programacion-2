96b547f51c89ae8b1388f59f9d5f2100bfaa03ad
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-05-25 18:10:34 -0300

    Modificacion Clase Usuario
    Implementacion de clase Usuario

--- proyecto-programacion2/src/holding/Usuario.java
+++ proyecto-programacion2/src/holding/Usuario.java
@@ -1,13 +1,28 @@
 package holding;
 
 public abstract class Usuario {
+    private static int CONTADOR = 0;
+    private int codigoUsuario;
     private String nombre;
     private String direccion;
     private String pass;
 
-    public Usuario(String nombre, String direccion, String pass) {
+    public Usuario( String nombre, String direccion, String pass) {
+        CONTADOR++;
+        this.codigoUsuario = CONTADOR;
         this.nombre = nombre;
         this.direccion = direccion;
         this.pass = pass;
+    }
+
+    public boolean validarNombre(String nombre){
+        return this.nombre == nombre ? true : false;
+    }
+    public boolean validarPass(String pass){
+        return this.pass == pass ? true : false;
+    }
+
+    public int getCodigo(){
+        return this.codigoUsuario;
     }
 }
