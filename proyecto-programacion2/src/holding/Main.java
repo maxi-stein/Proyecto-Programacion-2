f0d1ea1b219b6e0bb3e8e75bc89801b3784fa5a2
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-05-26 14:50:45 -0300

    Se modifica el main

--- proyecto-programacion2/src/holding/Main.java
+++ proyecto-programacion2/src/holding/Main.java
@@ -2,7 +2,7 @@ package holding;
 
 public class Main {
     public static void main(String[] args) {
-        SistemaDeGestion.run();
-
+        SistemaDeGestion sg = new SistemaDeGestion();
+        sg.run();
     }
 }
