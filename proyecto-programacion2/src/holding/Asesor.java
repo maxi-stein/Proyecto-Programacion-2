f45e35f3761a9454293e06d1a7c92a09c6eb1457
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-05-26 14:49:25 -0300

    Agregar funcionalidad a Admin, Asesor y Vendedor

--- proyecto-programacion2/src/holding/Asesor.java
+++ proyecto-programacion2/src/holding/Asesor.java
@@ -7,10 +7,26 @@ public class Asesor extends Usuario{
     private String titulacion;
     private LocalDate fechaInicio;
     private ArrayList<AREAS_MERCADO> mercadosCubiertos;
+    private ArrayList<Empresa> empresasAsesoradas;
 
     public Asesor(String nombre, String direccion, String pass, String titulacion, LocalDate fechaInicio) {
         super(nombre, direccion, pass);
         this.titulacion = titulacion;
         this.fechaInicio = fechaInicio;
+    }
+
+    @Override
+    public void proceder() {
+
+    }
+
+    @Override
+    public int mostrarMenu() {
+        return 0;
+    }
+
+    @Override
+    public int leerNum() {
+        return 0;
     }
 }
