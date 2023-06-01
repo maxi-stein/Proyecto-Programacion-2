aeb4f737d23d4a5c26db5409671994e084ad1c6d
MDL-Proyectos <matias.adilella@gmail.com>
2023-06-01 20:18:13 -0300

    Modif. de Proceder y Menu de Admin|

--- proyecto-programacion2/src/holding/Admin.java
+++ proyecto-programacion2/src/holding/Admin.java
@@ -7,7 +7,37 @@ public class Admin extends Usuario {
 
     @Override
     public void proceder() {
+        int opcion = mostrarMenu();
+        while(opcion != 9){
+            switch (opcion){
+                case 1:
+                    mostrarInformacion();
+                    break;
+                case 2: //crear admin
 
+                    break;
+                case 3: //crear vendedor
+                         
+                    break;
+                case 4: //crear asesor
+
+                    break;
+                case 5: //crear empresa
+
+                    break;
+                case 6: //asignar vendedor captado
+
+                    break;
+                case 7: //crear ciudad
+
+                    break;
+                case 8: //crear País
+
+                    break;
+
+            }
+            if(opcion != 9) opcion = mostrarMenu();
+        }
     }
 
     @Override
@@ -15,16 +45,18 @@ public class Admin extends Usuario {
         System.out.println("Datos de Usuario: "+ super.toString());
     }
 
+
+
     @Override
     public int mostrarMenu() {
         int opcion = 0;
-        while (opcion>8 || opcion<1) {
+        while (opcion>9 || opcion<1) {
             System.out.printf("1-Mostrar datos de Usuario \t2-Crear Administrador " +
                     "\n3-Crear Vendedor \t4-Crear Asesor" +
                     "\n5-Crear Empresa \t6-Asignar Vendedor Captado " +
-                    "\n7- Crear Ciudad \t8-Crear Pais");
+                    "\n7- Crear Ciudad \t8-Crear Pais \t 9-Salir");
             opcion = leerNum();
-            if(opcion>8 || opcion<1) System.out.println("Opcion incorrecta, intente de nuevo.");
+            if(opcion>9 || opcion<1) System.out.println("Opcion incorrecta, intente de nuevo.");
         }
         return opcion;
     }
