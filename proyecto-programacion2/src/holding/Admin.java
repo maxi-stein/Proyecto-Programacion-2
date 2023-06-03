efe76939234f4ff2e709d97b294029ce1be42805
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-06-03 13:57:48 -0300

    Implementacion Menu Opciones

--- proyecto-programacion2/src/holding/Admin.java
+++ proyecto-programacion2/src/holding/Admin.java
@@ -14,7 +14,7 @@ public class Admin extends Usuario {
 
                     break;
                 case 2:
-
+                    mostrarABMUsuario();
                     break;
                 case 3:
                          
@@ -28,7 +28,7 @@ public class Admin extends Usuario {
                 case 6:
                     break;
             }
-            if(opcion != 9) opcion = mostrarMenu();
+            if(opcion != 6) opcion = mostrarMenu();
         }
     }
 
@@ -41,18 +41,51 @@ public class Admin extends Usuario {
         int opcion = 0;
         while (opcion>6 || opcion<1) {
             System.out.printf("1-Mostrar datos del Administrador \t2-ABM Usuarios" +
-                    "\n3-ABM Empresas \t4-ABM Ciudad y Pais" +
-                    "\n5-ABM Areas de Mercado \t6-Salir");
+                    "\n3-ABM Empresas \t4-ABM Areas de Mercado "+
+                    "\n5-Crear Ciudad - Pais \t6-Salir");
             opcion = Consola.leerEntero();
         }
         return opcion;
     }
-    public int mostrarAbmUsuario(){
+    public void mostrarABMUsuario(){
+        int opcion = 0;
+        while (opcion>11 || opcion<1) {
+            System.out.printf("1-Crear Administrador \t2-Modificar Administrador \t3-Eliminar Administrador" +
+                    "\n4-Agregar Vendedor \t5-Agregar Vendedor Captado \t6-Modificar Vendedor \t7-Eliminar Vendedor" +
+                    "\n8-Agregar Asesor \t9-Modificar Asesor \t10-Eliminar Asesor" +
+                    "\n11-Salir");
+            opcion = Consola.leerEntero();
+        }
+        switch (opcion){
+            case 1:
+                break;
+        }
+    }
+
+    public int mostrarABMEmpresas(){
+        int opcion = 0;
+        while (opcion>13 || opcion<1) {
+            System.out.printf("1-Crear Empresa \t2-Modificar Empresa \t3-Eliminar Empresa" +
+                    "\n4-Agregar Vendedor \t5-Despedir Vendedor "+
+                    "\n6-Agregar Asesor \t7-Despedir Asesor" +
+                    "\n8-Agregar Area de Mercado \t9-Eliminar Area de Mercado" +
+                    "\n10-Agregar Ciudad \t11-Quitar Ciudad \t12-Asignar Sede" +
+                    "\n13-Salir");
+            opcion = Consola.leerEntero();
+        }
+        return opcion;
+    }
+
+    public void crearCiudad(){
+        System.out.println("Elegir el Pais origen:");
+        //implementacion faltante
+    }
+
+    public int mostrarABMAreasMercado(){
         int opcion = 0;
         while (opcion>6 || opcion<1) {
-            System.out.printf("1-Crear Administrador \t2-Modificar Administrador \t3-Eliminar Administrador" +
-                    "\n4-Agregar Vendedor \t5-Modificar Vendedor \t6-Eliminar Vendedor" +
-                    "\n7-Agregar Asesor \t8-Modificar Asesor \t9-Eliminar Asesor");
+            System.out.printf("1-Crear Area Mercado \t2-Modificar Area Mercado" +
+                    "\n3-Salir");
             opcion = Consola.leerEntero();
         }
         return opcion;
