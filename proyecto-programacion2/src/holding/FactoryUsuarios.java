8b9f4bcb47b3f2f8d7454bbc0fce3f3f3e60b7a3
Maximiliano Stein <maxi_stein23@hotmail.com>
2023-06-20 19:26:43 -0300

    Modificacion diagram de clases

--- proyecto-programacion2/src/holding/FactoryUsuarios.java
+++ proyecto-programacion2/src/holding/FactoryUsuarios.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.HashMap;
 
 public class FactoryUsuarios {
-    private static BaseDeDatosSingleton bd = BaseDeDatosSingleton.getInstance();
     public static void crearUsuario(int opcion) {
         if(opcion == 1){
             crearAdmin();
@@ -23,14 +22,14 @@ public class FactoryUsuarios {
         Admin a = new Admin();
         a.pedirDatosBasicos();
         a.mostrarCredenciales();
-        bd.agregarUsuario(a);
+        BaseDeDatosSingleton.agregarUsuario(a);
     }
     private static void crearVendedor(){
         Vendedor v = new Vendedor();
         v.pedirDatosBasicos();
         System.out.println("Seleccione la empresa en la que trabaja: ");
-        bd.listarEmpresas();
-        HashMap<Integer, Empresa> empresas = bd.obtenerEmpresas();
+        BaseDeDatosSingleton.listarEmpresas();
+        HashMap<Integer, Empresa> empresas = BaseDeDatosSingleton.obtenerEmpresas();
         int key = 0;
         do {
             key = Consola.leerEntero();
@@ -39,7 +38,7 @@ public class FactoryUsuarios {
         v.setEmpresaTrabajo(empresaTrabajo);
         empresaTrabajo.agregarVendedor(v);
         v.mostrarCredenciales();
-        bd.agregarUsuario(v);
+        BaseDeDatosSingleton.agregarUsuario(v);
     }
     private static void crearAsesor(){
         Asesor asesor = new Asesor();
@@ -49,8 +48,8 @@ public class FactoryUsuarios {
         asesor.setTitulacion(titulacion);
 
         System.out.println("Elija el area de mercado que asesora");
-        bd.listarAreasDeMercado();
-        HashMap <Integer,AreasMercado> areas = bd.obtenerAreasDeMercado();
+        BaseDeDatosSingleton.listarAreasDeMercado();
+        HashMap <Integer,AreasMercado> areas = BaseDeDatosSingleton.obtenerAreasDeMercado();
         Integer keyArea = 0;
         do {
             keyArea = Consola.leerEntero();
@@ -61,8 +60,8 @@ public class FactoryUsuarios {
 
         //se agrega la empresa para la que trabaja y se establece VinculacionAsesorEmpresa
         System.out.println("Seleccione la empresa que asesora: ");
-        bd.listarEmpresas();
-        HashMap<Integer, Empresa> empresas = bd.obtenerEmpresas();
+        BaseDeDatosSingleton.listarEmpresas();
+        HashMap<Integer, Empresa> empresas = BaseDeDatosSingleton.obtenerEmpresas();
         int keyEmpresa = 0;
         boolean compatible;
         do {
@@ -75,8 +74,8 @@ public class FactoryUsuarios {
         System.out.println("Ingresa la fecha de inicio (dd/mm/aaaa): ");
         LocalDate fechaInicio = Consola.leerFecha();
         asesor.mostrarCredenciales();
-        bd.agregarAsesorAEmpresa(keyEmpresa,asesor,fechaInicio);//se agrega el asesor a la empresa
-        bd.agregarUsuario(asesor);//se agrega el asesor a la base de datos de usuarios
+        BaseDeDatosSingleton.agregarAsesorAEmpresa(keyEmpresa,asesor,fechaInicio);//se agrega el asesor a la empresa
+        BaseDeDatosSingleton.agregarUsuario(asesor);//se agrega el asesor a la base de datos de usuarios
     }
 
     private static void crearVendedorCaptado(){
@@ -84,8 +83,8 @@ public class FactoryUsuarios {
         Vendedor vendedorCaptado = new Vendedor();
         vendedorCaptado.pedirDatosBasicos();
         System.out.println("Determine el vendedor maestro: ");
-        bd.listarVendedores();
-        vendedores = bd.obtenerVendedores();
+        BaseDeDatosSingleton.listarVendedores();
+        vendedores = BaseDeDatosSingleton.obtenerVendedores();
         int opcion = Consola.leerEntero();
         while (!vendedores.containsValue(opcion)){
             System.out.println("Ingrese una opcion correcta");
@@ -96,7 +95,7 @@ public class FactoryUsuarios {
         vendedorCaptado.setEmpresaTrabajo(vendedorMaestro.getEmpresaTrabajo());//le asigno la empresa de trabajo al nuevo vendedor
         vendedorMaestro.captarVendedor(vendedorCaptado); //se asocia el vendedor captado con su vendedor maestro
         vendedorCaptado.mostrarCredenciales();
-        bd.agregarUsuario(vendedorCaptado); //se agrega el vendedor a la base de datos
+        BaseDeDatosSingleton.agregarUsuario(vendedorCaptado); //se agrega el vendedor a la base de datos
 
     }
 
